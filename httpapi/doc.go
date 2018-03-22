8792fad47b6920f46be080405a3738da57d86239
Peter Bourgon <peter@bourgon.org>
2018-03-22 14:19:25 -0700

    httpapi: clarify doc comment

--- httpapi/doc.go
+++ httpapi/doc.go
@@ -1,2 +1,3 @@
-// Package httpapi provides HTTP interfaces to domain objects.
+// Package httpapi provides HTTP interfaces to CASPaxos domain objects.
+// Generally it wraps types from package extension rather than package protocol.
 package httpapi
